af5a99f21910f1c7a06d50543d35b0a38a825401
Alexa201228 <aleksandra.grid2012@gmail.com>
2022-01-30 14:34:32 +0300

    add schemas and migrations

--- models.py
+++ models.py
@@ -1,7 +1,8 @@
-from sqlalchemy import Column, Integer, String
-from sqlalchemy.orm import relationship
+from sqlalchemy import Column, Integer, String, DateTime, ForeignKey
+from sqlalchemy.orm import relationship, backref
+from sqlalchemy.schema import CheckConstraint
 
-from .configs.database import Base
+from configs.database import Base
 
 
 class Role(Base):
@@ -12,8 +13,37 @@ class Role(Base):
     role_description = Column(String)
 
 
+class BookNumber(Base):
+
+    __tablename__ = 'book_number'
+
+    number_id = Column(Integer, primary_key=True, index=True)
+    arrival_date = Column(DateTime(timezone=True))
+    leaving_date = Column(DateTime(timezone=True))
+
+    __table_args__ = (
+        CheckConstraint('arrival_date < leaving_date'),
+    )
+
+
 class Room(Base):
 
     __tablename__ = 'room'
 
     room_id = Column(Integer, primary_key=True, index=True)
+    price_for_night = Column(Integer)
+    book_numbers = relationship('BookNumber', backref=backref('room', lazy='dynamic'))
+
+
+class User(Base):
+
+    __tablename__ = 'user'
+
+    user_id = Column(Integer, primary_key=True, index=True)
+    email = Column(String, unique=True)
+    first_name = Column(String)
+    last_name = Column(String)
+    role_id = Column(Integer, ForeignKey('role.role_id'))
+
+    role = relationship('Role', backref=backref('users', lazy='dynamic'))
+
---
+++ schemas.py
@@ -0,0 +1,25 @@
+from pydantic import BaseModel
+
+
+class Role(BaseModel):
+    id: int
+    description: str
+
+
+class UserBase(BaseModel):
+    id: int
+    email: EmailStr
+    role: Role
+    
+    class Config:
+        orm_mode = True
+        
+
+class RoomBase(BaseModel):
+    id: int
+
+
+class UserIn(UserBase):
+    first_name: str
+    last_name: str
+    
