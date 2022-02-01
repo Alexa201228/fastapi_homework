6f15549ab3c629d98006f559af53e0eb5c437955
aleksandra.gridneva <aleksandra.gridneva@simbirsoft.com>
2022-02-01 17:34:31 +0300

    add enter endpoint

--- configs/database.py
+++ configs/database.py
@@ -1,10 +1,11 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
-
+import databases
 
 SQL_DATABASE_URL = 'sqlite:///./sqlite_db.db'
 
+database = databases.Database(SQL_DATABASE_URL)
 db_engine = create_engine(SQL_DATABASE_URL, connect_args={'check_same_thread': False})
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=db_engine)
--- main.py
+++ main.py
@@ -1,5 +1,23 @@
 from fastapi import FastApi
+from fastapi_sqlalchemy import DBSessionMiddleware
 
+from configs.database import database, SQL_DATABASE_URL
 
 app = FastApi()
 
+app.add_middleware(DBSessionMiddleware, db_url=SQL_DATABASE_URL)
+
+
+@app.on_event("startup")
+async def startup():
+    await database.connect()
+
+
+@app.on_event("shutdown")
+async def shutdown():
+    await database.disconnect()
+
+
+@app.get('/')
+async def start():
+    return {'hello': 'db_service world'}
